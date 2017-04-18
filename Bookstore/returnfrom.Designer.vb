4fa7a4f451c979cf83432c1e9eba2522a668cd86
JP4R7 <artpp1996@gmail.com>
2017-04-18 12:50:00 +0700

    debug_returnform
    ดึงค่าจากฐานข้อมูลได้ เหลือคิดค่าปรับหากคืนเกินกำหนด

--- Bookstore/returnfrom.Designer.vb
+++ Bookstore/returnfrom.Designer.vb
@@ -1,9 +1,9 @@
-﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
 Partial Class returnfrom
     Inherits System.Windows.Forms.Form
 
     'Form overrides dispose to clean up the component list.
-    <System.Diagnostics.DebuggerNonUserCode()> _
+    <System.Diagnostics.DebuggerNonUserCode()>
     Protected Overrides Sub Dispose(ByVal disposing As Boolean)
         Try
             If disposing AndAlso components IsNot Nothing Then
@@ -20,26 +20,28 @@ Partial Class returnfrom
     'NOTE: The following procedure is required by the Windows Form Designer
     'It can be modified using the Windows Form Designer.  
     'Do not modify it using the code editor.
-    <System.Diagnostics.DebuggerStepThrough()> _
+    <System.Diagnostics.DebuggerStepThrough()>
     Private Sub InitializeComponent()
         Me.Label1 = New System.Windows.Forms.Label()
-        Me.TextBox1 = New System.Windows.Forms.TextBox()
-        Me.Button1 = New System.Windows.Forms.Button()
+        Me.txtSkey = New System.Windows.Forms.TextBox()
+        Me.btnS = New System.Windows.Forms.Button()
         Me.GroupBox1 = New System.Windows.Forms.GroupBox()
         Me.GroupBox2 = New System.Windows.Forms.GroupBox()
-        Me.CheckBox1 = New System.Windows.Forms.CheckBox()
+        Me.btnUpdate = New System.Windows.Forms.Button()
+        Me.txtFell = New System.Windows.Forms.TextBox()
         Me.timeReturn = New System.Windows.Forms.Label()
-        Me.TextBox2 = New System.Windows.Forms.TextBox()
-        Me.Button2 = New System.Windows.Forms.Button()
-        Me.DataGridView1 = New System.Windows.Forms.DataGridView()
-        Me.Column1 = New System.Windows.Forms.DataGridViewTextBoxColumn()
-        Me.Column2 = New System.Windows.Forms.DataGridViewTextBoxColumn()
-        Me.Column3 = New System.Windows.Forms.DataGridViewTextBoxColumn()
-        Me.Column4 = New System.Windows.Forms.DataGridViewTextBoxColumn()
-        Me.Column5 = New System.Windows.Forms.DataGridViewTextBoxColumn()
+        Me.CheckBox = New System.Windows.Forms.CheckBox()
+        Me.DataGridView = New System.Windows.Forms.DataGridView()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.txtBname = New System.Windows.Forms.TextBox()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.txtBID = New System.Windows.Forms.TextBox()
+        Me.Label5 = New System.Windows.Forms.Label()
+        Me.txtReDate = New System.Windows.Forms.TextBox()
         Me.GroupBox1.SuspendLayout()
         Me.GroupBox2.SuspendLayout()
-        CType(Me.DataGridView1, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.DataGridView, System.ComponentModel.ISupportInitialize).BeginInit()
         Me.SuspendLayout()
         '
         'Label1
@@ -51,130 +53,161 @@ Partial Class returnfrom
         Me.Label1.TabIndex = 0
         Me.Label1.Text = "รหัสสมาชิก"
         '
-        'TextBox1
+        'txtSkey
         '
-        Me.TextBox1.Location = New System.Drawing.Point(152, 19)
-        Me.TextBox1.Name = "TextBox1"
-        Me.TextBox1.Size = New System.Drawing.Size(100, 20)
-        Me.TextBox1.TabIndex = 1
+        Me.txtSkey.Location = New System.Drawing.Point(152, 19)
+        Me.txtSkey.Name = "txtSkey"
+        Me.txtSkey.Size = New System.Drawing.Size(100, 20)
+        Me.txtSkey.TabIndex = 1
         '
-        'Button1
+        'btnS
         '
-        Me.Button1.Location = New System.Drawing.Point(295, 15)
-        Me.Button1.Name = "Button1"
-        Me.Button1.Size = New System.Drawing.Size(113, 23)
-        Me.Button1.TabIndex = 2
-        Me.Button1.Text = "แสดงรายการยืม"
-        Me.Button1.UseVisualStyleBackColor = True
+        Me.btnS.Location = New System.Drawing.Point(295, 15)
+        Me.btnS.Name = "btnS"
+        Me.btnS.Size = New System.Drawing.Size(113, 23)
+        Me.btnS.TabIndex = 2
+        Me.btnS.Text = "แสดงรายการยืม"
+        Me.btnS.UseVisualStyleBackColor = True
         '
         'GroupBox1
         '
-        Me.GroupBox1.Controls.Add(Me.Button1)
+        Me.GroupBox1.Controls.Add(Me.btnS)
         Me.GroupBox1.Controls.Add(Me.Label1)
-        Me.GroupBox1.Controls.Add(Me.TextBox1)
+        Me.GroupBox1.Controls.Add(Me.txtSkey)
         Me.GroupBox1.Location = New System.Drawing.Point(83, 12)
         Me.GroupBox1.Name = "GroupBox1"
         Me.GroupBox1.Size = New System.Drawing.Size(479, 51)
         Me.GroupBox1.TabIndex = 3
         Me.GroupBox1.TabStop = False
-        Me.GroupBox1.Text = "GroupBox1"
         '
         'GroupBox2
         '
-        Me.GroupBox2.Controls.Add(Me.Button2)
-        Me.GroupBox2.Controls.Add(Me.TextBox2)
+        Me.GroupBox2.Controls.Add(Me.Label5)
+        Me.GroupBox2.Controls.Add(Me.txtReDate)
+        Me.GroupBox2.Controls.Add(Me.Label4)
+        Me.GroupBox2.Controls.Add(Me.txtBID)
+        Me.GroupBox2.Controls.Add(Me.Label3)
+        Me.GroupBox2.Controls.Add(Me.txtBname)
+        Me.GroupBox2.Controls.Add(Me.Label2)
+        Me.GroupBox2.Controls.Add(Me.btnUpdate)
+        Me.GroupBox2.Controls.Add(Me.txtFell)
         Me.GroupBox2.Controls.Add(Me.timeReturn)
-        Me.GroupBox2.Controls.Add(Me.CheckBox1)
+        Me.GroupBox2.Controls.Add(Me.CheckBox)
         Me.GroupBox2.Location = New System.Drawing.Point(12, 70)
         Me.GroupBox2.Name = "GroupBox2"
         Me.GroupBox2.Size = New System.Drawing.Size(638, 92)
         Me.GroupBox2.TabIndex = 4
         Me.GroupBox2.TabStop = False
-        Me.GroupBox2.Text = "GroupBox2"
         '
-        'CheckBox1
+        'btnUpdate
         '
-        Me.CheckBox1.AutoSize = True
-        Me.CheckBox1.Location = New System.Drawing.Point(438, 52)
-        Me.CheckBox1.Name = "CheckBox1"
-        Me.CheckBox1.Size = New System.Drawing.Size(41, 17)
-        Me.CheckBox1.TabIndex = 0
-        Me.CheckBox1.Text = "คืน"
-        Me.CheckBox1.UseVisualStyleBackColor = True
+        Me.btnUpdate.Location = New System.Drawing.Point(507, 35)
+        Me.btnUpdate.Name = "btnUpdate"
+        Me.btnUpdate.Size = New System.Drawing.Size(113, 23)
+        Me.btnUpdate.TabIndex = 3
+        Me.btnUpdate.Text = "ตกลง"
+        Me.btnUpdate.UseVisualStyleBackColor = True
+        '
+        'txtFell
+        '
+        Me.txtFell.Location = New System.Drawing.Point(438, 22)
+        Me.txtFell.Name = "txtFell"
+        Me.txtFell.Size = New System.Drawing.Size(53, 20)
+        Me.txtFell.TabIndex = 2
         '
         'timeReturn
         '
         Me.timeReturn.AutoSize = True
-        Me.timeReturn.Location = New System.Drawing.Point(435, 22)
+        Me.timeReturn.Location = New System.Drawing.Point(202, 57)
         Me.timeReturn.Name = "timeReturn"
         Me.timeReturn.Size = New System.Drawing.Size(44, 13)
         Me.timeReturn.TabIndex = 1
         Me.timeReturn.Text = "เวลาคืน"
         '
-        'TextBox2
+        'CheckBox
         '
-        Me.TextBox2.Location = New System.Drawing.Point(21, 19)
-        Me.TextBox2.Name = "TextBox2"
-        Me.TextBox2.Size = New System.Drawing.Size(100, 20)
-        Me.TextBox2.TabIndex = 2
+        Me.CheckBox.AutoSize = True
+        Me.CheckBox.Location = New System.Drawing.Point(438, 52)
+        Me.CheckBox.Name = "CheckBox"
+        Me.CheckBox.Size = New System.Drawing.Size(41, 17)
+        Me.CheckBox.TabIndex = 0
+        Me.CheckBox.Text = "คืน"
+        Me.CheckBox.UseVisualStyleBackColor = True
         '
-        'Button2
+        'DataGridView
         '
-        Me.Button2.Location = New System.Drawing.Point(507, 35)
-        Me.Button2.Name = "Button2"
-        Me.Button2.Size = New System.Drawing.Size(113, 23)
-        Me.Button2.TabIndex = 3
-        Me.Button2.Text = "ตกลง"
-        Me.Button2.UseVisualStyleBackColor = True
+        Me.DataGridView.AllowUserToAddRows = False
+        Me.DataGridView.AllowUserToDeleteRows = False
+        Me.DataGridView.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize
+        Me.DataGridView.Location = New System.Drawing.Point(33, 180)
+        Me.DataGridView.Name = "DataGridView"
+        Me.DataGridView.ReadOnly = True
+        Me.DataGridView.Size = New System.Drawing.Size(599, 262)
+        Me.DataGridView.TabIndex = 5
         '
-        'DataGridView1
+        'Label2
         '
-        Me.DataGridView1.AllowUserToAddRows = False
-        Me.DataGridView1.AllowUserToDeleteRows = False
-        Me.DataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize
-        Me.DataGridView1.Columns.AddRange(New System.Windows.Forms.DataGridViewColumn() {Me.Column1, Me.Column2, Me.Column3, Me.Column4, Me.Column5})
-        Me.DataGridView1.Location = New System.Drawing.Point(33, 180)
-        Me.DataGridView1.Name = "DataGridView1"
-        Me.DataGridView1.ReadOnly = True
-        Me.DataGridView1.Size = New System.Drawing.Size(599, 262)
-        Me.DataGridView1.TabIndex = 5
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(393, 25)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(39, 13)
+        Me.Label2.TabIndex = 4
+        Me.Label2.Text = "ค่าปรับ"
         '
-        'Column1
+        'txtBname
         '
-        Me.Column1.HeaderText = "Column1"
-        Me.Column1.Name = "Column1"
-        Me.Column1.ReadOnly = True
+        Me.txtBname.Location = New System.Drawing.Point(71, 22)
+        Me.txtBname.Name = "txtBname"
+        Me.txtBname.Size = New System.Drawing.Size(100, 20)
+        Me.txtBname.TabIndex = 3
         '
-        'Column2
+        'Label3
         '
-        Me.Column2.HeaderText = "Column2"
-        Me.Column2.Name = "Column2"
-        Me.Column2.ReadOnly = True
+        Me.Label3.AutoSize = True
+        Me.Label3.Location = New System.Drawing.Point(13, 25)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(52, 13)
+        Me.Label3.TabIndex = 5
+        Me.Label3.Text = "ชื่อหนังสือ"
         '
-        'Column3
+        'Label4
         '
-        Me.Column3.HeaderText = "Column3"
-        Me.Column3.Name = "Column3"
-        Me.Column3.ReadOnly = True
+        Me.Label4.AutoSize = True
+        Me.Label4.Location = New System.Drawing.Point(6, 55)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(58, 13)
+        Me.Label4.TabIndex = 7
+        Me.Label4.Text = "รหัสหนังสือ"
         '
-        'Column4
+        'txtBID
         '
-        Me.Column4.HeaderText = "Column4"
-        Me.Column4.Name = "Column4"
-        Me.Column4.ReadOnly = True
+        Me.txtBID.Location = New System.Drawing.Point(71, 52)
+        Me.txtBID.Name = "txtBID"
+        Me.txtBID.Size = New System.Drawing.Size(100, 20)
+        Me.txtBID.TabIndex = 6
         '
-        'Column5
+        'Label5
         '
-        Me.Column5.HeaderText = "Column5"
-        Me.Column5.Name = "Column5"
-        Me.Column5.ReadOnly = True
+        Me.Label5.AutoSize = True
+        Me.Label5.Location = New System.Drawing.Point(202, 25)
+        Me.Label5.Name = "Label5"
+        Me.Label5.Size = New System.Drawing.Size(56, 13)
+        Me.Label5.TabIndex = 9
+        Me.Label5.Text = "กำหนดคืน"
+        '
+        'txtReDate
+        '
+        Me.txtReDate.Location = New System.Drawing.Point(260, 22)
+        Me.txtReDate.Name = "txtReDate"
+        Me.txtReDate.Size = New System.Drawing.Size(100, 20)
+        Me.txtReDate.TabIndex = 8
         '
         'returnfrom
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
         Me.ClientSize = New System.Drawing.Size(664, 461)
-        Me.Controls.Add(Me.DataGridView1)
+        Me.Controls.Add(Me.DataGridView)
         Me.Controls.Add(Me.GroupBox2)
         Me.Controls.Add(Me.GroupBox1)
         Me.Name = "returnfrom"
@@ -183,24 +216,26 @@ Partial Class returnfrom
         Me.GroupBox1.PerformLayout()
         Me.GroupBox2.ResumeLayout(False)
         Me.GroupBox2.PerformLayout()
-        CType(Me.DataGridView1, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.DataGridView, System.ComponentModel.ISupportInitialize).EndInit()
         Me.ResumeLayout(False)
 
     End Sub
 
     Friend WithEvents Label1 As Label
-    Friend WithEvents TextBox1 As TextBox
-    Friend WithEvents Button1 As Button
+    Friend WithEvents txtSkey As TextBox
+    Friend WithEvents btnS As Button
     Friend WithEvents GroupBox1 As GroupBox
     Friend WithEvents GroupBox2 As GroupBox
-    Friend WithEvents Button2 As Button
-    Friend WithEvents TextBox2 As TextBox
+    Friend WithEvents btnUpdate As Button
+    Friend WithEvents txtFell As TextBox
     Friend WithEvents timeReturn As Label
-    Friend WithEvents CheckBox1 As CheckBox
-    Friend WithEvents DataGridView1 As DataGridView
-    Friend WithEvents Column1 As DataGridViewTextBoxColumn
-    Friend WithEvents Column2 As DataGridViewTextBoxColumn
-    Friend WithEvents Column3 As DataGridViewTextBoxColumn
-    Friend WithEvents Column4 As DataGridViewTextBoxColumn
-    Friend WithEvents Column5 As DataGridViewTextBoxColumn
+    Friend WithEvents CheckBox As CheckBox
+    Friend WithEvents DataGridView As DataGridView
+    Friend WithEvents Label2 As Label
+    Friend WithEvents txtBname As TextBox
+    Friend WithEvents Label4 As Label
+    Friend WithEvents txtBID As TextBox
+    Friend WithEvents Label3 As Label
+    Friend WithEvents Label5 As Label
+    Friend WithEvents txtReDate As TextBox
 End Class
--- Bookstore/returnfrom.vb
+++ Bookstore/returnfrom.vb
@@ -1,3 +1,89 @@
 ﻿Public Class returnfrom
+    Dim cdb As New conndb()
+    Dim strcell As String
+    Private Sub returnfrom_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        txtFell.Text = calFee(Date.FromOADate((15 / 4 / 2017)), Date.FromOADate((18 / 4 / 2017))).ToString
+    End Sub
+    Private Sub showdata()
+        cdb.mystr = "SELECT dbo.BorrowReturn.Borrow_ID, dbo.BorrowReturn.Borrow_Date, dbo.BorrowReturn.Return_Date, dbo.BorrowReturn.Fee, dbo.BorrowReturn.SSN, dbo.BorrowReturn.Book_ID, dbo.Book.Status
+                     FROM  dbo.Book INNER JOIN dbo.BorrowReturn ON dbo.Book.Book_ID = dbo.BorrowReturn.Book_ID WHERE dbo.BorrowReturn.SSN = '" + txtSkey.Text + "' "
+        cdb.myObjconn().Open()
+        cdb.myda() = New SqlClient.SqlDataAdapter(cdb.mystr, cdb.myObjconn)
+        cdb.myds() = New DataSet
+        cdb.myda().Fill(cdb.myds(), "Book")
 
+        DataGridView.DataMember = "Book"
+        DataGridView.DataSource = cdb.myds()
+        cdb.myObjconn().Close()
+    End Sub
+
+    Private Sub intotxt()
+        ctxt()
+        cdb.mystr = " SELECT        dbo.Book.Book_Name, dbo.Book.Book_ID, dbo.BorrowReturn.Return_Date FROM dbo.Book INNER JOIN dbo.BorrowReturn ON dbo.Book.Book_ID = dbo.BorrowReturn.Book_ID where dbo.BorrowReturn.Borrow_ID = '" + strcell.Trim + "';"
+        cdb.myObjconn.Open()
+        Dim cmd As New SqlClient.SqlCommand(cdb.mystr, cdb.myObjconn())
+        cdb.mydr = cmd.ExecuteReader()
+        If cdb.mydr.HasRows Then
+            While cdb.mydr.Read
+                txtBname.Text += cdb.mydr.Item(0)
+                txtBID.Text += cdb.mydr.Item(1)
+                txtReDate.Text += cdb.mydr.Item(2)
+            End While
+        Else
+            MessageBox.Show("ไม่มีข้อมูล", "ผลการดำเนินการ")
+        End If
+        cdb.mydr.Close()
+        cdb.myObjconn().Close()
+    End Sub
+    Private Sub ctxt()
+        txtReDate.Clear()
+        txtBID.Clear()
+        txtBname.Clear()
+
+    End Sub
+    Private Sub DataGridView_CellContentClick(sender As Object, e As DataGridViewCellEventArgs) Handles DataGridView.CellContentClick
+        With Me.DataGridView
+            .SelectionMode = DataGridViewSelectionMode.FullRowSelect
+            .MultiSelect = False
+        End With
+        strcell = DataGridView.Rows.Item(e.RowIndex).Cells(0).Value.ToString
+        intotxt()
+
+    End Sub
+
+    Private Sub btnS_Click(sender As Object, e As EventArgs) Handles btnS.Click
+        showdata()
+    End Sub
+
+    Private Sub btnUpdate_Click(sender As Object, e As EventArgs) Handles btnUpdate.Click
+        If CheckBox.Checked Then
+            DGUpdateBook()
+            MessageBox.Show("คืนสำเร็จ")
+            CheckBox.CheckState = CheckState.Unchecked
+        End If
+    End Sub
+
+    Private Function calFee(ByVal FRedate As Date, ByVal redate As Date) As Integer
+        Dim diff
+        diff = DateDiff(DateInterval.Day, FRedate, redate)
+        Return diff
+    End Function
+
+    Private Sub DGUpdateBook()
+        Dim connection As New Data.SqlClient.SqlConnection
+        Dim cmd As New Data.SqlClient.SqlCommand
+
+        connection.ConnectionString = cdb.myStrconn()
+        cmd.CommandText = "UPDATE [bookbd].[dbo].[Book] SET [Status] = @valueStatus WHERE [dbo].[Book].[Book_ID] = @BookID;"
+        cmd.Parameters.Add("@BookID", SqlDbType.VarChar)
+        cmd.Parameters.Add("@valueStatus", SqlDbType.VarChar)
+
+        connection.Open()
+        cmd.Connection = connection
+        cmd.Parameters("@BookID").Value = txtBID.Text
+        cmd.Parameters("@valueStatus").Value = "yes"
+
+        cmd.ExecuteNonQuery()
+
+    End Sub
 End Class
